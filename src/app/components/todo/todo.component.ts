3d6dc695c77142caabd7e23636f2dfbff0e514f9
oerol <erolxerolx@gmail.com>
2022-11-17 22:41:07 +0100

    Todos are now created dynamically

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  TODO_LIST = [{ title: 'Write a review on Goodreads' }, { title: 'Do some research on the author' }];
+  GOALS_LIST = [{ title: 'Read 10 pages' }];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
